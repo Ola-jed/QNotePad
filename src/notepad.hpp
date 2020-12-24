403b6fa21445dca59e1c108d33307b48c85ccb10
Ola __J <66482155+Ola-jed@users.noreply.github.com>
2020-12-24 10:49:30 +0100

    Create notepad.hpp

---
+++ src/notepad.hpp
@@ -0,0 +1,45 @@
+#ifndef NOTEPAD_HPP
+#define NOTEPAD_HPP
+
+#include <QMainWindow>
+#include <QString>
+#include <QPushButton>
+#include <QTextEdit>
+#include <QMessageBox>
+#include <QInputDialog>
+#include <QFile>
+#include <QFont>
+#include <QLabel>
+#include <QTextStream>
+#include <QStringList>
+#include <QFileDialog>
+#include <QColorDialog>
+#include <QColor>
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Notepad; }
+QT_END_NAMESPACE
+
+class Notepad : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    Notepad(QWidget *parent = nullptr);
+    ~Notepad();
+
+private:
+    Ui::Notepad *ui;
+    QString fileName = "";
+    bool isSaved = false;
+
+private slots:
+    void onNewFile();
+    void onOpenFile();
+    void onSaveFile();
+    void onQuit();
+    void onTextChanged();
+    void onColorChanged();
+};
+#endif // NOTEPAD_HPP
