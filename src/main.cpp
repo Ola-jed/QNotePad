cfe3ee8d49e10859dbfb153595a7d13d98a4da6d
Ola <66482155+Ola-jed@users.noreply.github.com>
2021-01-02 18:49:33 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include "notepad.hpp"
-#include <QApplication>
+#include <QScreen>
+#include <QStyle>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Notepad w;
+    w.setGeometry(QStyle::alignedRect(Qt::LeftToRight,Qt::AlignCenter,w.size(),QGuiApplication::primaryScreen()->availableGeometry()));
     w.show();
     return a.exec();
 }
